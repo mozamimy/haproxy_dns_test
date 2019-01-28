d7e5fc0b04d64fcc3a43f58b5c1b135af11698d6
mozamimy (Moza USANE) <alice@mozami.me>
2019-01-28 17:08:27 +0900

    Vovoy no voy

--- test.sh
+++ test.sh
@@ -2,6 +2,7 @@
 
 while :
 do
-  echo "1.6: $(curl -sS http://localhost:8016/test.txt), 1.7: $(curl -sS http://localhost:8017/test.txt), 1.8: $(curl -sS http://localhost:8018/test.txt)"
+  # echo "1.6: $(curl -sS http://localhost:8016/test.txt), 1.7: $(curl -sS http://localhost:8017/test.txt), 1.8: $(curl -sS http://localhost:8018/test.txt), envoy1.9: $(curl -sS http://localhost:8019/test.txt)"
+  echo "1.6: $(curl -sS http://localhost:8016/test.txt), envoy1.9: $(curl -sS http://localhost:8019/test.txt)"
   sleep 1
 done
